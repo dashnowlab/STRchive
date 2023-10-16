d171ace6ba3c0cdf0a939fdaa4fcb620e70747cb
Laurel (they/he) <laurel.hiatt@gmail.com>
2023-10-16 10:18:05 -0600

    adding commandline arguments

--- scripts/csv2json.py
+++ scripts/csv2json.py
@@ -1,16 +1,12 @@
 import csv
 import json
 import sys
+import argparse
 sys.path.append('../')
 import reftogeneorientation
 
-def main():
-    #Here we are defining the original CSV as well as the output csv/json
-    in_csv = 'data/STR-disease-loci.csv'
-    out_json = 'data/STR-disease-loci.processed.json'
-    out_csv = 'data/STR-disease-loci.processed.csv'
-
-    #This will create the gene orientation columns from ref orientation for each motif type
+def main(in_csv, out_csv, out_json):
+    # This will create the gene orientation columns from ref orientation for each motif type
     # Reference/benign, pathogenic, and unknown
     reftogeneorientation.process_csv(in_csv, out_csv)
 
@@ -24,4 +20,10 @@ def main():
         json_file.write(json.dumps(data, indent=4))
 
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser(description='Process CSV and convert to JSON')
+    parser.add_argument('--in_csv', default='data/STR-disease-loci.csv', help='Input CSV file path')
+    parser.add_argument('--out_csv', default='data/STR-disease-loci.processed.csv', help='Output CSV file path')
+    parser.add_argument('--out_json', default='data/STR-disease-loci.processed.json', help='Output JSON file path')
+
+    args = parser.parse_args()
+    main(args.in_csv, args.out_csv, args.out_json)
