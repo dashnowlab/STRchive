6dddc07f5c284fe5d4430be4f34045b25e323f89
Laurel (they/he) <laurel.hiatt@gmail.com>
2023-11-13 11:29:59 -0700

    cleaned up for all_publications

--- scripts/earliestpubdate.R
+++ scripts/earliestpubdate.R
@@ -52,114 +52,14 @@ consolidated_strings <- gene_info %>%
   summarize(consolidated_strings = paste(unique(c(hgnc_symbol, external_synonym)), collapse = ' OR ')) %>%
   pull(consolidated_strings)
 
-#gene_info <- gene_info[1:27,]
-perform_pubmed_query <- function(gene_info) {
 
 
-  for (i in seq_along(gene_names)) {
-    gene_name <- gene_names[i]
-    print(gene_name)
-    cat("Processing gene:", gene_name, "\n")
-
-    # Use str_detect to check if gene_name is present in each consolidated string
-    idx <- str_detect(consolidated_strings, regex(gene_name, ignore_case = TRUE))
-
-    # Filter out the relevant consolidated strings
-    or_terms <- paste(consolidated_strings[idx], collapse = ' OR ')
-
-    # Construct the query with 'AND' logic among [Title/Abstract] segments
-    query <- paste0('("repeat expansion"[Title/Abstract] OR "tandem repeat"[Title/Abstract] OR "repeat expansions"[Title/Abstract] OR "tandem repeats"[Title/Abstract]) AND (', or_terms, ')[Title/Abstract] AND "English"[Language] AND ("disease"[Title/Abstract] OR "disorder"[Title/Abstract] OR "diseases"[Title/Abstract] OR "disorders"[Title/Abstract] OR "syndrome"[Title/Abstract] OR "syndromes"[Title/Abstract]) AND "journal article"[Publication Type] NOT "review"[Publication Type]')
-
-    # Clean up any unnecessary slashes from the query
-    query <- gsub("\"", "", query, fixed = TRUE)
-    query <- gsub("  ", " ", query)  # Remove double spaces
-    #print(query)
-    #print(length(query))
-
-    # Perform PubMed search using web history
-    search_results <- entrez_search(db = "pubmed", term = query, retmax = 500)
-    myIDlist <- search_results
-    print(paste0("Search results are: ", search_results))
-    #search_results <- get_pubmed_ids(query, api_key = NULL)
-    print(paste0("Search results are: ", search_results))
-    articles <- fetch_pubmed_data(search_results, retstart = 0,
-                                  retmax = 500, format = "xml",
-                      encoding = "UTF8")
-    #if (!is.na(search_results) && search_results$count > 0)  {
-     # fetch_pubmed_data(search_results,
-      #
-       #
-        #
-         #
-      #article_ids <- entrez_search(db = "pubmed", term = query)$ids
-      #print(length(article_ids))
-      #articles <- entrez_fetch(db = "pubmed", id = article_ids, rettype = "xml", retmax = 10000)
-      publications[[gene_name]] <- articles
-    }
-
-  return(publications)
-}
-
-
-# Example usage:
-# gene_names <- unique(your_gene_info$hgnc_symbol)
-# result <- perform_pubmed_query(your_gene_info, gene_names)
-
-gene_info <- gene_info[4:10, ]
-gene_info_subset <- gene_info[4:27, ]
-pubmed_results <- perform_pubmed_query(gene_info)
-
-earliest_pub_dates_df <- data.frame(
-  Gene = character(0),
-  Earliest_Pub_Date = numeric(0),
-  NumPub = numeric(0),
-  Titles = I(list())
-)
-
-for (i in 1:length(pubmed_results)) {
-  gene <- unique(gene_info$hgnc_symbol)[i]
-
-  # Extract publication years for each gene using custom_grep
-  publication_years <- custom_grep(pubmed_results[[i]], "PubDate", "char")
-  publication_years <- str_extract(publication_years, "(?<=<Year>)\\d{4}(?=</Year>)")
-
-  # Convert years to numeric format
-  publication_years <- as.numeric(publication_years)
-
-  # Find the minimum publication year
-  min_year <- min(publication_years, na.rm = TRUE)
-
-  # Extract titles
-  titles <- custom_grep(pubmed_results[[i]], "ArticleTitle", "char")
-  print(titles)
-  # Count the number of titles
-  titles_count <- length(titles)
-
-  # Combine titles into a single string
-  titles_string <- paste(titles, collapse = ", ")
-
-  # Add values to the data frame
-  row_index <- nrow(earliest_pub_dates_df) + 1
-  earliest_pub_dates_df[row_index, ] <- list(gene, min_year, titles_count, I(titles_string))
-}
-
-
-write.table(earliest_pub_dates_df, "/Users/quinlan/Documents/Git/STRchive/scripts/earliest_pub_dates.tsv", sep = "\t", quote = FALSE, row.names = FALSE)
-
-write.table(earliest_pub_dates_df[, 1:3], "/Users/quinlan/Documents/Git/STRchive/scripts/earliest_pub_dates_only.tsv", sep = "\t", quote = FALSE, row.names = FALSE)
-
-
-
-
-
-
-all_publications <- list()
 
 base_directory <- '/Users/quinlan/Documents/Git/STRchive/data/'
 
 perform_pubmed_query <- function(gene_info) {
   gene_names <- unique(gene_info$hgnc_symbol)
-  all_publications <- list()  # Initialize the list to store all publications
+  file_paths <- list()  # Initialize the list to store all publications
   consolidated_strings <- gene_info %>%
     group_by(hgnc_symbol) %>%
     summarize(consolidated_strings = paste(unique(c(hgnc_symbol, external_synonym)), collapse = ' OR ')) %>%
@@ -167,7 +67,6 @@ perform_pubmed_query <- function(gene_info) {
 
   for (i in seq_along(gene_names)) {
     gene_name <- gene_names[i]
-    print(gene_name)
     cat("Processing gene:", gene_name, "\n")
 
     # Use str_detect to check if gene_name is present in each consolidated string
@@ -184,7 +83,7 @@ perform_pubmed_query <- function(gene_info) {
     query <- gsub("  ", " ", query)  # Remove double spaces
     print(query)
 
-    out_file <- paste0(gene_name)
+    out_file <- paste0(base_directory, gene_name)
 
     # Include a separator ("/") between base_directory and gene_name
     # Modify dest_file_prefix to include the full file path
@@ -193,20 +92,24 @@ perform_pubmed_query <- function(gene_info) {
                                    batch_size = 10000,
                                    dest_file_prefix = out_file,
                                    encoding = "ASCII")
+    # the function adds 01.txt so, gotta fix that here
+    out_file <- paste0(base_directory, gene_name, "01.txt")
+    print(out_file)
 
     # Check if the file was created successfully
+    cat("Full file path:", out_file, "\n")
     if (file.exists(out_file)) {
-      # Save the file path for later retrieval
-      all_publications[[gene_name]] <- out_file
+      cat("File exists.\n")
+      file_paths[[gene_name]] <- out_file
     } else {
-      cat("Error: File not created -", out_file, "\n")
+      cat("Error: File not found -", out_file, "\n")
     }
   }
 
-  return(all_publications)
+  return(file_paths)
 }
 
-pubmed_results <- perform_pubmed_query(gene_info)
+file_paths <- perform_pubmed_query(gene_info)
 
 
 publications_df <- data.frame(
@@ -215,13 +118,12 @@ publications_df <- data.frame(
   stringsAsFactors = FALSE
 )
 
-
-
+all_publications <- list()
 
 # Assuming file_paths is a list of file paths
 for (gene_name in names(file_paths)) {
   # Append "01.txt" to the file path
-  file_path <- paste0(file_paths[[gene_name]], "01.txt")
+  file_path <- paste0(file_paths[[gene_name]])
 
   tryCatch({
     # Read the file into a character vector
@@ -264,3 +166,29 @@ for (gene_name in all_publications) {
 
 # Now, publications_df contains the desired information
 
+for (i in 1:length(pubmed_results)) {
+  gene <- unique(gene_info$hgnc_symbol)[i]
+
+  # Extract publication years for each gene using custom_grep
+  publication_years <- custom_grep(pubmed_results[[i]], "PubDate", "char")
+  publication_years <- str_extract(publication_years, "(?<=<Year>)\\d{4}(?=</Year>)")
+
+  # Convert years to numeric format
+  publication_years <- as.numeric(publication_years)
+
+  # Find the minimum publication year
+  min_year <- min(publication_years, na.rm = TRUE)
+
+  # Extract titles
+  titles <- custom_grep(pubmed_results[[i]], "ArticleTitle", "char")
+  print(titles)
+  # Count the number of titles
+  titles_count <- length(titles)
+
+  # Combine titles into a single string
+  titles_string <- paste(titles, collapse = ", ")
+
+  # Add values to the data frame
+  row_index <- nrow(earliest_pub_dates_df) + 1
+  earliest_pub_dates_df[row_index, ] <- list(gene, min_year, titles_count, I(titles_string))
+}
