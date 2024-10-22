56f16e372204c80e0e69f8a9970bec8f3da011a0
Vincent Rubinetti <vince.rubinetti@gmail.com>
2024-10-21 23:40:40 -0400

    fix header active nav link

--- site/astro.config.mjs
+++ site/astro.config.mjs
@@ -3,4 +3,6 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   site: "https://strchive.org",
+  /** https://github.com/withastro/astro/issues/4190 */
+  trailingSlash: "never",
 });
