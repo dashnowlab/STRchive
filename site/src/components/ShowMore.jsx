20031f1fbe4a8390455fd574888ec0bdf2874137
Vincent Rubinetti <vince.rubinetti@gmail.com>
2024-11-14 16:01:17 -0500

    Add more info to locus pages (#61)
    Addresses #60 
    
    Note: `mechanism_detail` perhaps should be changed to
    `mechanism_details` in the data, to be consistent with e.g.
    `prevalence_details`

---
+++ site/src/components/ShowMore.jsx
@@ -0,0 +1,47 @@
+import { useEffect, useRef, useState } from "react";
+import classes from "./ShowMore.module.css";
+
+/** line limit */
+const lines = 2;
+
+/** collapse long lines of text */
+const ShowMore = ({ children }) => {
+  const ref = useRef();
+  const [enabled, setEnabled] = useState(false);
+  const [expanded, setExpanded] = useState(false);
+
+  /** on first render */
+  useEffect(() => {
+    if (!ref.current) return;
+    /** select text */
+    const range = document.createRange();
+    range.setStartBefore(ref.current);
+    range.setEndAfter(ref.current);
+    /** get number of lines */
+    const rects = range.getClientRects();
+    /** if line limit exceeded, enable component */
+    if (rects.length - 1 > lines) setEnabled(true);
+  }, []);
+
+  return (
+    <>
+      <span
+        ref={ref}
+        className={enabled && !expanded ? classes.box : undefined}
+        style={{ WebkitLineClamp: enabled && !expanded ? lines : undefined }}
+      >
+        {children}
+      </span>
+      {enabled && (
+        <button
+          className={classes.button}
+          onClick={() => setExpanded(!expanded)}
+        >
+          {expanded ? "Show less" : "Show more"}
+        </button>
+      )}
+    </>
+  );
+};
+
+export default ShowMore;
