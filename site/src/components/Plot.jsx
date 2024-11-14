01b9b96353b2de59b87bfbff68055deabcb1a195
Vincent Rubinetti <vince.rubinetti@gmail.com>
2024-11-14 18:39:46 -0500

    fix plot resizing

--- site/src/components/Plot.jsx
+++ site/src/components/Plot.jsx
@@ -1,15 +1,23 @@
 import { useEffect, useRef } from "react";
 import Plotly from "plotly.js-dist";
+import classes from "./Plot.module.css";
 
-const Plot = ({ data, layout }) => {
+const Plot = ({ data, layout, ...props }) => {
   const ref = useRef();
 
   useEffect(() => {
     if (!ref.current) return;
-    Plotly.newPlot(ref.current, { data, layout });
+    Plotly.newPlot(ref.current, {
+      data,
+      layout: {
+        ...layout,
+        margin: { l: 50, r: 50, b: 100, t: 50, pad: 4 },
+      },
+      config: { responsive: true },
+    });
   }, []);
 
-  return <div ref={ref}></div>;
+  return <div ref={ref} className={classes.plot} {...props}></div>;
 };
 
 export default Plot;
