b43eeb29b2a1991376cab102ed5387071988e6e8
hdashnow <hdashnow@users.noreply.github.com>
2024-12-13 18:12:16 +0000

    fix y height only

--- scripts/age-onset-json.py
+++ scripts/age-onset-json.py
@@ -123,11 +123,10 @@ def build_JSON(args):
         name="Age 18"
     ))
 
-    fig.update_layout(width=800,
-                    height=len(diseases)*20,
+    fig.update_layout(height=len(diseases)*18,
                     template="plotly_white",
                     legend_title="Inheritance",
-                    margin=dict(l=10, r=10, t=10, b=10)
+                    margin=dict(l=10, r=10, t=10, b=10),
                     ) 
     fig.update_xaxes(title_text="Age of onset (years)", range=[-10,100])
     fig.update_yaxes(title_text="Disease")
