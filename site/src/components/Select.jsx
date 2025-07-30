92cc6d64310f9e22e0efc41b3dfbc1e4393237ff
Vincent Rubinetti <vince.rubinetti@gmail.com>
2025-07-30 16:26:40 -0400

    add caret back to select component

--- site/src/components/Select.jsx
+++ site/src/components/Select.jsx
@@ -1,17 +1,23 @@
+import { FaAngleDown } from "react-icons/fa";
+import classes from "./Select.module.css";
+
 /** dropdown select with label */
 const Select = ({ label, options, onChange, ...props }) => (
   <label>
     {label}
-    <select
-      {...props}
-      onChange={(event) => onChange?.(event.target.value, event)}
-    >
-      {options.map(({ value, label }, index) => (
-        <option key={index} value={value}>
-          {label}
-        </option>
-      ))}
-    </select>
+    <div className={classes.container}>
+      <select
+        {...props}
+        onChange={(event) => onChange?.(event.target.value, event)}
+      >
+        {options.map(({ value, label }, index) => (
+          <option key={index} value={value}>
+            {label}
+          </option>
+        ))}
+      </select>
+      <FaAngleDown className={classes.icon} />
+    </div>
   </label>
 );
 
