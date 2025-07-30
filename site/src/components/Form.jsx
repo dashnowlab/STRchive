003455386b23e0746d5613b1717afeb6f9da2a97
Vincent Rubinetti <vince.rubinetti@gmail.com>
2025-07-30 14:52:29 -0400

    form wrapper component

---
+++ site/src/components/Form.jsx
@@ -0,0 +1,36 @@
+/** form wrapper around set of fields */
+const Form = ({ onSubmit, children, ...props }) => (
+  <form
+    style={{ display: "contents" }}
+    onSubmit={(event) => {
+      /** prevent page navigation */
+      event.preventDefault();
+
+      /** get data from form */
+      const form = event.currentTarget;
+      const formData = new FormData(form);
+
+      const data = {};
+
+      /** transform form data into nicer format */
+      for (const [key, value] of formData.entries()) {
+        /** if we can parse as number, do it */
+        if (
+          typeof value === "string" &&
+          value.trim() &&
+          !Number.isNaN(Number(value))
+        )
+          data[key] = Number(value);
+        else
+          /** raw (string) value */
+          data[key] = String(value);
+      }
+
+      /** call callback with data */
+      onSubmit(data);
+    }}
+    {...props}
+  />
+);
+
+export default Form;
