51a49c7fc5a507427e1d7425c8278b7b097f90bc
Vincent Rubinetti <vince.rubinetti@gmail.com>
2025-08-05 20:00:17 -0400

    update cloud readme

--- site/cloud/deploy.sh
+++ site/cloud/deploy.sh
@@ -1,22 +1,24 @@
 #!/bin/bash
 # run from /site
 
-script=$1
+name=$1
 project=gap-som-dbmi-strchive-app-60
-function=strchive-$script
+function=strchive-$name
+build=cloudrunfunction-builder
+run=cloudrunfunction-runtime
 region=us-central1
 
 source .env.local
 github=$GITHUB_TOKEN
 
 gcloud run deploy $function \
-  --source ./scripts/$script \
+  --source ./cloud/$name \
   --function entrypoint \
   --base-image nodejs22 \
   --region $region \
   --project $project \
-  --build-service-account projects/$project/serviceAccounts/cloudrunfunction-builder@$project.iam.gserviceaccount.com \
-  --service-account cloudrunfunction-runtime@$project.iam.gserviceaccount.com \
+  --build-service-account projects/$project/serviceAccounts/$build@$project.iam.gserviceaccount.com \
+  --service-account $run@$project.iam.gserviceaccount.com \
   --max-instances 2 \
   --no-invoker-iam-check \
   --set-env-vars GITHUB_TOKEN=$github
