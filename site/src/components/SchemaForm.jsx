1926224a8c7761ada956c571ad16969f45089af7
Vincent Rubinetti <vince.rubinetti@gmail.com>
2025-09-02 16:50:25 -0400

    start transition to json-schema-library

--- site/src/components/SchemaForm.jsx
+++ site/src/components/SchemaForm.jsx
@@ -1,70 +1,231 @@
-import { Fragment, useRef, useState } from "react";
+import { cloneElement, Fragment, useMemo, useState } from "react";
 import { FaArrowDown, FaArrowUp, FaPlus, FaTrash } from "react-icons/fa6";
 import { LuSend } from "react-icons/lu";
 import clsx from "clsx";
-import { uniq, upperFirst } from "lodash-es";
-import { useDebounceFn } from "@reactuses/core";
-import Form from "@rjsf/core";
+import { compileSchema, draft2020, extendDraft } from "json-schema-library";
+import { cloneDeep, groupBy, uniq, upperFirst } from "lodash-es";
+import { get, set, split } from "@sagold/json-pointer";
 import Button from "@/components/Button";
 import Heading from "@/components/Heading";
 import Help from "@/components/Help";
 import NumberBox from "@/components/NumberBox";
 import Select from "@/components/Select";
 import TextBox from "@/components/TextBox";
-import { validator } from "@/data/schema";
+import { makeList } from "@/util/format";
+import Form from "./Form";
 import classes from "./SchemaForm.module.css";
 
 /** form automatically generated from json schema */
-const SchemaForm = ({ schema, sections, data: initialData = {} }) => {
-  const ref = useRef(null);
-  const [formData, setFormData] = useState(initialData);
+const SchemaForm = ({ schema, data: initialData }) => {
+  /** compile schema */
+  const compiled = useMemo(
+    () => compileSchema(schema, { drafts: [draft] }),
+    [schema],
+  );
 
-  /** revalidate whole form */
-  const validate = useDebounceFn(() => ref.current?.validateForm(), 300);
+  /** if no initial data, generate blank values */
+  initialData ??= compiled.getData(null, { extendDefaults: false });
 
-  /** create func with debounced revalidation of whole form */
-  /** https://github.com/rjsf-team/react-jsonschema-form/issues/3616#issuecomment-3221419763 */
-  const triggerValidate =
-    (func) =>
-    (...args) => {
-      func(...args);
-      validate.run();
+  /** current form data state */
+  const [data, setData] = useState(initialData);
+
+  /** revalidate when data changes */
+  const { errors } = useMemo(() => {
+    compiled.context.data = data;
+    return compiled.validate(data);
+  }, [compiled, data]);
+
+  /** all nested fields */
+  const fields = useMemo(() => {
+    /** compile schema into "nodes" */
+    const nodes = compiled.toSchemaNodes();
+
+    /** return path up schema tree to root */
+    const toRoot = (node) => {
+      const path = [];
+      while (node.parent) {
+        path.push(node.parent);
+        node = node.parent;
+      }
+      return path;
     };
 
+    /** if section undefined, get from closest parent */
+    for (const node of nodes)
+      node.schema.section ??= toRoot(node).map(
+        (node) => node.schema.section,
+      )[0];
+
+    /** group by section */
+    return Object.entries(groupBy(nodes, "schema.section"))
+      .map(([section, fields]) => ({ section, fields }))
+      .filter(({ section }) => section !== "undefined");
+  }, [compiled]);
+
   return (
-    <Form
-      ref={ref}
-      className={classes.form}
-      /** full form data */
-      formData={formData}
-      onChange={(event) => setFormData(event.formData)}
-      /** allow certain info to be accessed from any field */
-      formContext={{
-        fullSchema: schema,
-        fullData: formData,
-        sections,
-        triggerValidate,
-      }}
-      /** shape of data */
-      schema={schema}
-      /** custom validator */
-      validator={validator}
-      /** hide summary list of errors */
-      showErrorList={false}
-      /** never validate automatically, only on demand by calling validateForm */
-      noValidate
-      /** custom layouts */
-      templates={{
-        ObjectFieldTemplate,
-        ArrayFieldTemplate,
-        FieldTemplate,
-        FieldErrorTemplate,
-      }}
-      /** custom fields */
-      fields={{ StringField, NumberField }}
-      onError={console.error}
-      onSubmit={({ formData }) => console.info(formData)}
-    >
+    <Form className={classes.form} onSubmit={console.info}>
+      {fields.map(({ section, fields }) => (
+        <section key={section}>
+          <Heading level={2}>{section}</Heading>
+
+          {fields.map((field, index) => {
+            /** schema props */
+            const {
+              type,
+              title,
+              description,
+              examples,
+              pattern,
+              enum: _enum,
+              minimum,
+              maximum,
+              less_than,
+              greater_than,
+            } = field.schema;
+
+            /** path to field, without root part */
+            const path = field.evaluationPath.replace(/^#\/properties/, "");
+
+            /** normalize type to array */
+            const types = [type].flat();
+
+            /** form data name */
+            const name = split(field.evaluationPath).pop();
+
+            /** help tooltip to show */
+            const tooltip = [
+              description,
+              ...(examples?.length ? [" "] : []),
+              examples?.length ? `Examples: ${examples.join(", ")}` : "",
+            ]
+              .filter(Boolean)
+              .join("<br/>");
+
+            /** is the field required to be set */
+            const required =
+              schema.required.includes(name) && types.includes("null");
+
+            /** full label elements to show */
+            const label = (
+              <>
+                {title ?? name ?? ""}
+                {tooltip && <Help>{tooltip}</Help>}
+              </>
+            );
+
+            /** get nested data value from path */
+            const value = get(data, path);
+
+            /** set nested data value from path */
+            const onChange = (value) =>
+              setData(set(cloneDeep(data), path, value));
+
+            /** get validation errors associated with this field */
+            const fieldErrors = errors.filter(({ data }) =>
+              data.pointer.includes(path),
+            );
+            /** is there an error on this field */
+            const isError = !!fieldErrors.length;
+
+            /** error component to show */
+            const error = isError ? (
+              <span className={classes.error}>
+                {fieldErrors
+                  .map(({ code, data, message }) => {
+                    /** prettify error message */
+                    if (code === "pattern-error")
+                      return (
+                        <>
+                          Must match pattern <code>{data.pattern}</code>
+                        </>
+                      );
+                    if (code === "enum-error")
+                      return <>Must be {makeList(data.schema.enum, "code")}</>;
+                    if (code === "compare-value-error")
+                      return <>{data.schema.message}</>;
+                    return message;
+                  })
+                  .map((error, index) => (
+                    <Fragment key={index}>{error}</Fragment>
+                  ))}
+              </span>
+            ) : null;
+
+            /** field control to show */
+            let control = <span></span>;
+
+            /** ref function */
+            const ref = (el) => {
+              if (!el || !("setCustomValidity" in el)) return;
+              /** also set native browser form validity to get various built-in features */
+              if (isError) el?.setCustomValidity("Error with this field");
+              else el?.setCustomValidity("");
+            };
+
+            /** single select */
+            if (_enum?.length) {
+              const options = uniq(["", ..._enum.filter(Boolean)]).map(
+                (value) => ({
+                  value,
+                  label: upperFirst(value),
+                }),
+              );
+              control = (
+                <Select
+                  options={options}
+                  value={value || ""}
+                  onChange={onChange}
+                />
+              );
+            } else if (types.includes("string"))
+              /** text input */
+              control = (
+                <TextBox
+                  pattern={pattern}
+                  value={value || ""}
+                  onChange={(value) => onChange(value || null)}
+                />
+              );
+            else if (types.includes("number") || types.includes("integer")) {
+              /** min value allowed to be input */
+              const min = Math.max(
+                ...[minimum, data[greater_than]].filter(
+                  (value) => typeof value === "number",
+                ),
+              );
+              /** max value allowed to be input */
+              const max = Math.min(
+                ...[maximum, data[less_than]].filter(
+                  (value) => typeof value === "number",
+                ),
+              );
+
+              control = (
+                <NumberBox
+                  name={name}
+                  label={label}
+                  required={required}
+                  pattern={pattern}
+                  step={types.includes("integer") ? 1 : "any"}
+                  min={Number.isFinite(min) ? min : undefined}
+                  max={Number.isFinite(max) ? max : undefined}
+                  value={value}
+                  onChange={onChange}
+                />
+              );
+            }
+
+            return (
+              <Fragment key={index}>
+                {/* props that should be on all fields */}
+                {cloneElement(control, { ref, name, label, required })}
+                {error}
+              </Fragment>
+            );
+          })}
+        </section>
+      ))}
+
       <section>
         <Button type="submit" design="bubble">
           <LuSend />
@@ -77,7 +238,6 @@ const SchemaForm = ({ schema, sections, data: initialData = {} }) => {
 
 export default SchemaForm;
 
-/** @param {import("@rjsf/utils").ObjectFieldTemplateProps} props */
 const ObjectFieldTemplate = ({
   idSchema,
   schema,
@@ -120,7 +280,6 @@ const ObjectFieldTemplate = ({
     </>
   );
 
-/** @param {import("@rjsf/utils").ArrayFieldTemplateProps} props */
 const ArrayFieldTemplate = ({
   schema,
   items,
@@ -220,149 +379,48 @@ const ArrayFieldTemplate = ({
   );
 };
 
-/** @param {import("@rjsf/utils").FieldTemplateProps} props */
-const FieldTemplate = ({ children, errors }) => (
-  <>
-    {children}
-    {errors}
-  </>
-);
-
-/** @param {import("@rjsf/utils").FieldErrorProps} props */
-const FieldErrorTemplate = ({ errors }) =>
-  errors?.length ? (
-    <div className={classes.error}>
-      {errors
-        .map((error) => upperFirst(error))
-        .join(", ")
-        .replaceAll(/(\w),(\w)/g, "$1 | $2")}
-    </div>
-  ) : null;
-
-/** @param {import("@rjsf/utils").WidgetProps} props */
-const StringField = ({
-  schema,
-  name,
-  formData: value,
-  onChange,
-  formContext: { fullSchema, triggerValidate },
-}) => {
-  const types = getTypes(schema);
-  const required = getRequired(schema, fullSchema);
-  const tooltip = getTooltip(schema);
-  const label = getLabel(schema, tooltip);
-
-  /** modify onChange func */
-  onChange = triggerValidate(onChange);
-
-  /** get different fields on property */
-  const { pattern, enum: _enum } = schema;
-
-  /** single select */
-  if (_enum)
-    return (
-      <Select
-        name={name}
-        label={label}
-        required={required}
-        options={uniq(["", ..._enum.filter(Boolean)]).map((value) => ({
-          value,
-          label: upperFirst(value),
-        }))}
-        value={value || ""}
-        onChange={onChange}
-      />
-    );
-
-  /** text input */
-  if (types.includes("string"))
-    return (
-      <TextBox
-        name={name}
-        label={label}
-        required={required}
-        pattern={pattern}
-        value={value || ""}
-        onChange={onChange}
-      />
-    );
+/** add custom keyword for validating one value less than other value */
+const lessThan = {
+  id: "lessThan",
+  keyword: "less_than",
+  addValidate: () => true,
+  validate: ({ node, data, pointer }) => {
+    const thisValue = data;
+    const fullData = node.context.data;
+    const otherKey = node.schema.less_than;
+    const otherValue = get(fullData, otherKey);
+    if (thisValue <= otherValue) return;
+    return node.createError("compare-value-error", {
+      pointer,
+      value: otherValue,
+      schema: { ...node.schema, message: `Must be less than ${otherKey}` },
+    });
+  },
 };
 
-/** @param {import("@rjsf/utils").WidgetProps} props */
-const NumberField = ({
-  schema,
-  name,
-  formData: value,
-  onChange,
-  formContext: { fullSchema, fullData, triggerValidate },
-}) => {
-  const types = getTypes(schema);
-  const required = getRequired(schema, fullSchema);
-  const tooltip = getTooltip(schema);
-  const label = getLabel(schema, tooltip);
-
-  /** modify onChange func */
-  onChange = triggerValidate(onChange);
-
-  /** get different fields on property */
-  const {
-    pattern,
-    enum: _enum,
-    minimum,
-    maximum,
-    less_than,
-    greater_than,
-  } = schema;
-
-  /** min value allowed to be input */
-  const min = Math.max(
-    ...[minimum, fullData[greater_than]].filter(
-      (value) => typeof value === "number",
-    ),
-  );
-  /** max value allowed to be input */
-  const max = Math.min(
-    ...[maximum, fullData[less_than]].filter(
-      (value) => typeof value === "number",
-    ),
-  );
-
-  return (
-    <NumberBox
-      name={name}
-      label={label}
-      required={required}
-      pattern={pattern}
-      step={types.includes("integer") ? 1 : "any"}
-      min={Number.isFinite(min) ? min : undefined}
-      max={Number.isFinite(max) ? max : undefined}
-      value={value}
-      onChange={onChange}
-    />
-  );
+/** add custom keyword for validating one value greater than other value */
+const greaterThan = {
+  id: "greaterThan",
+  keyword: "greater_than",
+  addValidate: () => true,
+  validate: ({ node, data, pointer }) => {
+    const thisValue = data;
+    const fullData = node.context.data;
+    const otherKey = node.schema.greater_than;
+    const otherValue = get(fullData, otherKey);
+    if (thisValue >= otherValue) return;
+    return node.createError("compare-value-error", {
+      pointer,
+      value: otherValue,
+      schema: { ...node.schema, message: `Must be greater than ${otherKey}` },
+    });
+  },
 };
 
-/** normalize type to array */
-const getTypes = ({ type }) => [type].flat();
-
-/** make help tooltip to show */
-const getTooltip = ({ description, examples }) =>
-  [
-    description,
-    ...(examples?.length ? [" "] : []),
-    examples?.length ? `Examples: ${examples.join(", ")}` : "",
-  ]
-    .filter(Boolean)
-    .join("<br/>");
-
-/** is the field required to be set */
-const getRequired = ({ name, type }, { required }) =>
-  required.includes(name) && !getTypes(type).includes("null");
-
-/** full label elements to show */
-const getLabel = ({ title, name }, tooltip) => (
-  <>
-    {title ?? name ?? ""}
-    {tooltip && <Help>{tooltip}</Help>}
-  </>
-);
+/** https://www.npmjs.com/package/json-schema-library#user-content-extending-a-draft */
+const draft = extendDraft(draft2020, {
+  /** match all schema */
+  $schemaRegEx: ".*",
+  /** add custom validation rules */
+  keywords: [lessThan, greaterThan],
+});
---
+++ site/src/util/format.jsx
@@ -0,0 +1,24 @@
+import { Fragment } from "react/jsx-runtime";
+
+/** make english list from array */
+export const makeList = (items, Wrapper = "span", joiner = "or") => {
+  items = items.map((item, index) => <Wrapper key={index}>{item}</Wrapper>);
+  if (!items?.length) return <></>;
+  if (items.length === 1) return items[0];
+  const first = items.slice(0, -1);
+  const last = items.slice(-1);
+  if (items.length === 2)
+    return (
+      <>
+        {first} {joiner} {last}
+      </>
+    );
+  return (
+    <>
+      {first.map((item, index) => (
+        <Fragment key={index}>{item}, </Fragment>
+      ))}
+      {joiner} {last}
+    </>
+  );
+};
