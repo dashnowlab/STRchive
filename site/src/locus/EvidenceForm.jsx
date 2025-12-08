a469ec6d7edeff3ec854f27d954ba869cc1d7973
Vincent Rubinetti <vince.rubinetti@gmail.com>
2025-12-08 11:17:36 -0500

    create page

--- site/src/locus/EvidenceForm.jsx
+++ site/src/locus/EvidenceForm.jsx
@@ -1,6 +1,6 @@
 import { useMemo } from "react";
 import { FaXmark } from "react-icons/fa6";
-import { LuFeather, LuSend } from "react-icons/lu";
+import { LuBookCheck, LuSend } from "react-icons/lu";
 import { cloneDeep, isEqual, omitBy, startCase } from "lodash-es";
 import { useLocalStorage } from "@reactuses/core";
 import { createPR } from "@/api/pr";
@@ -15,44 +15,44 @@ import { repo } from "@/layouts/meta";
 import { useQuery } from "@/util/hooks";
 import { shortenUrl } from "@/util/string";
 import loci from "~/STRchive-loci.json";
-import schema from "~/STRchive-loci.schema.json";
+import schema from "~/STRtr-kit-evidence.schema.json";
 
-/** add extra fields for edit metadata */
+/** add extra fields for evidence metadata */
 schema.properties = {
-  "edit-name": {
+  "evidence-name": {
     section: "Overview",
     ...contactSchema.name,
     type: ["string", "null"],
     default: "",
   },
-  "edit-username": {
+  "evidence-username": {
     section: "Overview",
     ...contactSchema.username,
     type: ["string", "null"],
     pattern: "^@.+",
     default: "",
   },
-  "edit-email": {
+  "evidence-email": {
     section: "Overview",
     ...contactSchema.email,
     type: ["string", "null"],
     default: "",
   },
-  "edit-title": {
+  "evidence-title": {
     section: "Overview",
-    title: "Edit Title",
+    title: "Evidence Title",
     description: "Succinct title describing these changes",
     examples: ["Fix mechanism details", "Update disease onset information"],
     type: "string",
     default: null,
   },
-  "edit-description": {
+  "evidence-description": {
     section: "Overview",
-    title: "Edit Description",
+    title: "Evidence Description",
     description:
       "Summary of changes, justification for changes, uncertainty in literature, or anything else we should know for review. Please be detailed. Provide at least 2-3 sentences.",
     examples: [
-      "Currently, the disease mechanism details cite a recently retracted paper doi:123456. This edit corrects the reference and updates...",
+      "Currently, the disease mechanism details cite a recently retracted paper doi:123456. This evidence corrects the reference and updates...",
     ],
     multiline: true,
     type: ["string", "null"],
@@ -61,21 +61,21 @@ schema.properties = {
   ...schema.properties,
 };
 
-schema.required.push("edit-title");
+schema.required.push("evidence-title");
 
-/** new/edit locus form */
-const EditForm = ({ heading, locus }) => {
+/** new/evidence locus form */
+const EvidenceForm = ({ heading, locus }) => {
   /** confirm with user before leaving page */
   // window.onbeforeunload = () => "";
 
   /** unique storage key for this page and form */
-  const storageKey = `edit-locus-${locus?.id ?? "new"}`;
+  const storageKey = `evidence-locus-${locus?.id ?? "new"}`;
 
   /** form data state */
   let [data, setData] = useLocalStorage(storageKey, {
-    ["edit-title"]: null,
-    ["edit-description"]: null,
-    ...cloneDeep(locus),
+    ["evidence-title"]: null,
+    ["evidence-description"]: null,
+    id: locus?.id ?? null,
   });
 
   /** was data loaded from storage */
@@ -91,61 +91,14 @@ const EditForm = ({ heading, locus }) => {
     data: response,
     status,
   } = useQuery(async () => {
-    /** pr body */
-    const name = data["edit-name"];
-    const username = data["edit-username"];
-    const email = data["edit-email"];
-    const description = data["edit-description"];
-    const body = [{ name, username, email }, { description }]
-      .map((group) =>
-        Object.entries(group)
-          .map(([key, value]) => [
-            `**${startCase(key)}**`,
-            value?.trim() ? value.trim() : "\\-",
-          ])
-          .flat()
-          .join("\n"),
-      )
-      .join("\n\n");
-
-    /** branch name from locus id */
-    const branch = data.id;
-
-    /** pr title */
-    const title = data["edit-title"];
-
-    /** remove edit metadata */
-    data = omitBy(cloneDeep(data), (value, key) => key.startsWith("edit-"));
-
-    /** merge with locus data */
-    const newLoci = cloneDeep(loci).map((locus) =>
-      locus.id === data.id ? data : locus,
-    );
-
-    /** pr files to change */
-    const files = [
-      {
-        path: "data/STRchive-loci.json",
-        content: JSON.stringify(newLoci, null, 2),
-      },
-    ];
-
-    return await createPR({
-      owner: "dashnowlab",
-      repo: "STRchive",
-      branch,
-      title,
-      body,
-      files,
-      labels: ["locus-edit"],
-    });
+    console.info(data);
   });
 
   return (
     <Form onSubmit={submit}>
       <section>
         <Heading level={1}>
-          <LuFeather />
+          <LuBookCheck />
           {heading}
         </Heading>
 
@@ -175,15 +128,7 @@ const EditForm = ({ heading, locus }) => {
         schema={schema}
         data={data}
         onChange={setData}
-        sections={[
-          "Overview",
-          "ID",
-          "Disease",
-          "Locus",
-          "Alleles",
-          "IDs",
-          "References",
-        ]}
+        sections={["Overview", "Evidence"]}
         onSubmit={(data) => submit(data)}
       />
 
@@ -211,4 +156,4 @@ const EditForm = ({ heading, locus }) => {
   );
 };
 
-export default EditForm;
+export default EvidenceForm;
