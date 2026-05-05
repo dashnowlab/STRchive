5dfc803dabd88d782883c17daf9d041f40454a59
Vincent Rubinetti <vince.rubinetti@gmail.com>
2026-05-05 17:29:08 -0400

    Add parse markdown util func (#349)
    ## Minor Changes
    
    - Add util func to parse markdown strings and render them to html
    - Add autolink literal extension
    - Make curation description field support markdown
    
    ## Checklist
    
    - [x] All changes are well summarized
    - [x] Check that the website preview looks good
    - [ ] Update the STRchive version in `CITATION.cff`, format X.Y.Z. If
    any major changes, increment Y. If only minor changes, increment Z. If
    the breaking change (rare), increment X.
    - [x] Ask someone to review this PR

---
+++ site/src/util/markdown.js
@@ -0,0 +1,11 @@
+import { micromark } from "micromark";
+import {
+  gfmAutolinkLiteral,
+  gfmAutolinkLiteralHtml,
+} from "micromark-extension-gfm-autolink-literal";
+
+export const parse = (content = "") =>
+  micromark(content, {
+    extensions: [gfmAutolinkLiteral()],
+    htmlExtensions: [gfmAutolinkLiteralHtml()],
+  });
